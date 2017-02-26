6a073379454184f789aa19d269130695db264012
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2017-02-26 23:50:38 +0600

    Comments for lib/cck_forms/parameter_type_class/boolean|checkboxes.rb

--- lib/cck_forms/parameter_type_class/boolean.rb
+++ lib/cck_forms/parameter_type_class/boolean.rb
@@ -1,3 +1,5 @@
+# Represents a single checkbox.
+#
 class CckForms::ParameterTypeClass::Boolean
   include CckForms::ParameterTypeClass::Base
 
@@ -5,18 +7,22 @@ class CckForms::ParameterTypeClass::Boolean
     'Галочка (да/нет)'
   end
 
+  # Is it true?
   def value?
     value.present? && value != '0'
   end
 
+  # Anything -> boolean
   def mongoize
     value?
   end
 
+  # 'yes/no' string
   def to_s(options = nil)
     value? ? 'да' : 'нет'
   end
 
+  # Checkbox HTML
   def build_form(form_builder, options)
     set_value_in_hash options
     form_builder.check_box :value, options.merge(value: 1, checked: value?)
