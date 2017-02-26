8bc6147f785465ef174ebd19671aacbbd2d8dd51
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2017-02-26 21:27:25 +0600

    Comments for lib/cck_forms/base.rb

--- lib/cck_forms/parameter_type_class/base.rb
+++ lib/cck_forms/parameter_type_class/base.rb
@@ -1,53 +1,46 @@
-# Базовая примесь для всех типов полей. Определяет всякие помогайки. Ее нужно включать методом include во все типы,
-# например, String, Checkboxes и т. п.
+# Base module for all field types. Is included in type classes like Album, Image etc.
 #
 #   class CckForms::ParameterTypeClass::NewType
 #     include CckForms::ParameterTypeClass::Base
-#
-#     def self.name
-#       'Новый тип'
-#     end
 #   end
 #
-#   CckForms::ParameterTypeClass::NewType.name            # 'Новый тип', берется из CckForms::ParameterTypeClass::NewType::name
-#
-# Использование типов:
+# Standalone usage of real classes:
 #
 #   field :cover_photo, type: CckForms::ParameterTypeClass::Image
-#   field :gallery, type: CckForms::ParameterTypeClass::Album
+#   field :gallery,     type: CckForms::ParameterTypeClass::Album
 #   field :description, type: CckForms::ParameterTypeClass::Text
 #
-# Что есть в базовом типе:
+# Base module includes:
 #
-#   1) все помощники УРЛов вида edit_article_path, включаемые через include Rails.application.routes.url_helpers;
+#   1) URL helpers like edit_article_path accessible via include Rails.application.routes.url_helpers;
 #
-#   2) методы cck_param и value, которые возвращают текущий параметр (module CckForms::*::Parameter) и его значение;
+#   2) methods cck_param & value returning current CCK parameter (module CckForms::*::Parameter) and his current value;
 #
-#   3) методы with_cck_param(param) do ... и with_value(value) do ..., которые устанавливают соотв, значения методов
-#      cck_param/value на время выполнения блока (еще есть with_cck_param_and_value(param, value) do ...);
+#   3) methods with_cck_param(param) do ..., with_value(value) do ... and with_cck_param_and_value(param, value) do ...
+#      which set the corresponding values for the block duration;
 #
-#   4) динамический метод (через method_missing и respond_to?) ..._with_value(value, args*), который делает то же,
-#      что и with_value, но для вызова 1 метода;
+#   4) dynamic method (via method_missing & respond_to?) ..._with_value(value, args*) which is basically the same as
+#      with_value do... but for one method invocation only: foo_with_value(v) <=> with_value(v) { foo }
 #
-#   5) set_value_in_hash(hash), который кладет value в hash[:value];
+#   5) utility set_value_in_hash(hash) placing value in hash[:value];
 #
-#   6) помощники для получения ХТМЛ ID form(_builder)?_name_to_id;
+#   6) utilities to get HTML ID: form(_builder)?_name_to_id;
 #
-#   7) методы для потребителей (типизированных объектов):
+#   7) methods to be consumed by the type classes:
 #
-#        self.code            - код типа из имени модуля (CckForms::ParameterType::RichText -> rich_text)
-#        self.name            - имя типа ("Cтрока")
+#        self.code  - type code (e.g. rich_text for CckForms::ParameterType::RichText)
+#        self.name  - type name (e.g. "A text string")
 #
 module CckForms::ParameterTypeClass::Base
   extend ActiveSupport::Concern
 
   included do
-    # Кое-где понадобятся помогайки-пути, сразу их включим.
     include Rails.application.routes.url_helpers
 
     attr_accessor :value
     attr_reader :valid_values_class_name, :cck_parameter
 
+    # Store options into instance variables like @valid_values and so on
     def initialize(options)
       options = options.symbolize_keys
 
@@ -70,14 +63,20 @@ module CckForms::ParameterTypeClass::Base
 
 
   module ClassMethods
+    # Called on a class to construct its instance from a MongoDB Hash.
+    # By default simply create a new object.
     def demongoize(something_from_database)
       new value: demongoize_value(something_from_database)
     end
 
+    # Only converts a value from MongoDB to its in-memory (Ruby) form.
+    # By default, return the value itself.
     def demongoize_value(value, parameter_type_class=nil)
       value
     end
 
+    # Called on an class to get a MongoDB Hash form of an instance object.
+    # By default simply calls mongoize of the instance.
     def mongoize(object)
       case object
       when self then object.mongoize
@@ -87,35 +86,30 @@ module CckForms::ParameterTypeClass::Base
       end
     end
 
-    # Возвращает строку с яваскриптовым кодом для выполнения в БД кода emit операции map-reduce на поле данного типа.
-    # Поскольку у различных типов данные в БД хранятся по-разному, и вообще понятие "текущего значения этого типа"
-    # различается, мы вынуждены отдельно описывать операцию emit, там где это необходимо. Смысл в том, чтобы этот метод
-    # вызвал emit для каждого хранимого значения.
+    # Returns Javascript emit function body to be used as a part of map/reduce "emit" step, see
+    # http://docs.mongodb.org/manual/applications/map-reduce/
     #
-    # Пример: есть поле city типа checkboxes, хранящее список городов, то-есть объекты, описываемые этим полем,
-    # могут быть в разных городах. Если мы хотим сделать группировочный запрос (частный случай map-reduce) в БД, чтобы,
-    # например, подсчитать кол-во объектов в городах, мы не можем просто сделать emit для всего поля city, поскольку
-    # оно содержит список городов. Мы должны вызывать emit для каждого элемента массива (т. е., для каждого идентификатора
-    # города). Генерацией этого кода и занимается этот метод.
+    # The reason for this is every type class has its own notion of "current value" and stores it specifically. Say,
+    # Checkboxes store an array of values and if we want to get distinct values we need a way to extract each "single
+    # value" from this array.
     #
-    # В частности, он используется при подсчете популярных значения различных полей (сколько объявлений, поданых
-    # в разные города, и т. п.)
+    # Example: imagine a field "city" of type Checkboxes. To make an aggregate query (a subtype of map-reduce)
+    # to count objects in different cities, for example, we can not run emit for the field as a whole since it is an
+    # array. We must call emit for each array value, that is for each city ID. This method does exactly this.
     #
-    # По-умолчанию, считаем, что значение, хранимое в поле "#{feild_name}" атомарно, и вызываем  emit для него.
+    # In particular, it is used in counting popular values (like "give me a list of cities sorted by the number of
+    # host objects (ads? companies?) in them").
     #
-    # Подробнее про map-reduce см. http://docs.mongodb.org/manual/applications/map-reduce/
+    # By default considers a value in "#{feild_name}" atomic and call emit for it.
     def emit_map_reduce(feild_name)
       field_name = 'this.' + feild_name
       "if(#{field_name} && #{field_name} != '') emit(#{field_name}, 1)"
     end
 
-    # Конвертирует имя элемента формы в ID, например facility[cck_params][1][value] -> facility_cck_params_1_value.
+    # Converts input name intp HTML ID, e.g. facility[cck_params][1][value] -> facility_cck_params_1_value.
     def form_name_to_id(name)
       name.gsub(/\]\[|[^-a-zA-Z0-9:.]/, '_').sub(/_\z/, '')
     end
-
-    # Методы, которые нужны наследникам (CckForms::ParameterTypeClass::*) для отображения, сортировки и прочей настройки самих типов.
-    # Эти методы не нужны обычным объектам - пользователям типов.
 
     # CckForms::ParameterTypeClass::Checkboxes -> 'checkboxes'
     # CckForms::ParameterTypeClass::RichText -> 'rich_text'
@@ -123,7 +117,7 @@ module CckForms::ParameterTypeClass::Base
       self.to_s.demodulize.underscore
     end
 
-    # Имя типа, например, для select>option[value]
+    # A type name, e.g. "A text string"
     def name
       nil
     end
@@ -131,7 +125,7 @@ module CckForms::ParameterTypeClass::Base
 
 
 
-  # Загрузит все классы-наследники.
+  # Load all type classes
   # TODO: relies on all classes to reside in this class' directory
   def self.load_type_classes
     return if @type_classes_loaded
@@ -146,10 +140,9 @@ module CckForms::ParameterTypeClass::Base
 
 
 
-  # "Нормальные" методы, которые будут доступны всем пользователям данного модуля.
+  # Usual methods available to type classes (consumers of this module)
 
-  # -> [[key1, value1], [key2, value2], ...]
-  # Нужен для построения ХТМЛ СЕЛЕКТов.
+  # Generates valid_values in form consumable to SELECT helper builders: [[key1, value1], [key2, value2], ...]
   def valid_values_enum
     valid_values = self.valid_values
     return [] if valid_values.blank?
@@ -161,13 +154,12 @@ module CckForms::ParameterTypeClass::Base
     result
   end
 
-  # -> "georgian: грузинская, albanian: албанская"
-  # Нужен для построения ХТМЛа и строк.
+  # Generates valid values as a comma-separated string: "georgian: грузинская, albanian: албанская" (for HTML puproses)
   def valid_values_as_string
     valid_values_enum.map { |enum| "#{enum[1]}: #{enum[0]}" }.join "\n"
   end
 
-  # Чтобы получать данные из форм и сохранять в БД. Использовать во вьюхах:
+  # Convert HTML form back into Hash again:
   #   = f.text_field :valid_values_as_string
   def valid_values_as_string=(string)
     new_valid_values = {}
@@ -178,13 +170,12 @@ module CckForms::ParameterTypeClass::Base
     self.valid_values = new_valid_values
   end
 
-  # Вернет класс, указанный как valid_values_class_name.
   # "City" -> City
   def valid_values_class
     if valid_values_class_name.present?
       if valid_values_class_name.is_a? Class
         valid_values_class_name
-      else # если это не строка, то пусть будет выборошено исключение
+      else # raises exception if this is not a string
         valid_values_class_name.constantize
       end
     else
@@ -192,13 +183,13 @@ module CckForms::ParameterTypeClass::Base
     end
   end
 
-  # Существует ли valid_values_class?
+  # Is valid_values_class exist at all?
   def valid_values_class?
     not valid_values_class.nil?
   end
 
-  # Если valid_values пустой, и есть valid_values_class, запишет в valid_values все значения
-  # из valid_values_class. Считает, что класс похож на ActiveRecord, и значения берет из all.
+  # If valid_values is empty and valid_values_class is not, extracts all values from this class into valid_values.
+  # Makes use of ActiveRecord-like method .all for this
   def valid_values
     @valid_values ||= begin
       if vv_class = valid_values_class
@@ -209,39 +200,43 @@ module CckForms::ParameterTypeClass::Base
     end
   end
 
-  # Строит форму редактирования. Просто input:text со всеми переданными опциями (значение можно определить через
-  # options[:value]).
+  # Builds an edit form in HTML.
+  # By default an input:text (value can be set via options[:value]).
   def build_form(form_builder, options)
     set_value_in_hash options
     form_builder.text_field :value, options
   end
 
-  # Вернем в виде HTML
+  # HTML form of a type class.
+  # By default to_s.
   def to_html(options = nil)
     to_s options
   end
 
-  # Чтобы принимать аргумент options
+  # Redefines to allow passing of options.
+  # By default, call to_s on the current value.
   def to_s(options = nil)
     value.to_s
   end
 
-  # Отображение для страниц "было-стало" в админках и пр. (например, тип "карта" можнет вернуть ХТМЛ картинки-миниатюры).
+  # Was-became HTML for admin panels etc. (like a type image with map preview for Map).
   def to_diff_value(options = nil)
     to_html options
   end
 
-  # Формируем поисковый запрос для монго на основе запроса (типа мини-DSL язык запроса, свой для каждого типа).
+  # Transforms DSL-like query (specific to each type) into Mongoid query.
+  # By default use simple where(field: query.to_s).
   def search(selectable, field, query)
     selectable.where(field => query.to_s)
   end
 
-  # Нужен для Rails.application.routes.url_helpers
+  # For Rails.application.routes.url_helpers
   def default_url_options
     {}
   end
 
-  # Реализация перобразования в/из Монго по умолчанию: берем то, что в value
+  # Transforms the value into MongoDB form.
+  # By default returns the value itself.
   def mongoize
     value
   end
@@ -263,12 +258,12 @@ module CckForms::ParameterTypeClass::Base
     options[:value] = value unless options.has_key? :value
   end
 
-  # См. ClassMethod.form_name_to_id
+  # See ClassMethod.form_name_to_id
   def form_name_to_id(name)
     self.class.form_name_to_id name
   end
 
-  # Конвертирует имя элемента формы из FormBuilder, см. form_name_to_id.
+  # Converts FormBuilder name to ID, see form_name_to_id
   def form_builder_name_to_id(form_builder, suffix = '')
     form_name_to_id([form_builder.options[:namespace], form_builder.object_name].compact.join('_') + suffix)
   end
