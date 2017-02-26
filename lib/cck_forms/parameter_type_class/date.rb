daf7e27f0364142063125dc441ea249fa0c2b056
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2017-02-26 23:56:35 +0600

    Comments for lib/cck_forms/parameter_type_class/date|date_range|date_time.rb

--- lib/cck_forms/parameter_type_class/date.rb
+++ lib/cck_forms/parameter_type_class/date.rb
@@ -1,3 +1,5 @@
+# Represents a single date.
+#
 class CckForms::ParameterTypeClass::Date
   include CckForms::ParameterTypeClass::Base
   include CckForms::DateTime
@@ -6,11 +8,13 @@ class CckForms::ParameterTypeClass::Date
     'Дата'
   end
 
+  # Date SELECT
   def build_form(form_builder, options)
     set_value_in_hash options
     self.class.build_date_form(form_builder, options)
   end
 
+  # Date SELECT as a set of 3 SELECTS: year, month, date
   def self.build_date_form(form_builder, options, type = '')
     val =  options[:value].is_a?(Hash) ? options[:value][type] : options[:value]
     val = CckForms::ParameterTypeClass::Time::date_object_from_what_stored_in_database(val)
@@ -19,6 +23,7 @@ class CckForms::ParameterTypeClass::Date
     ('<div class="form-inline">%s</div>' % form_builder.fields_for(:value) { |datetime_builder| datetime_builder.date_select type, form_element_options, form_element_html}).html_safe
   end
 
+  # "12.12.2012"
   def to_s(options = nil)
     if value.is_a? Time
       the_value = {
