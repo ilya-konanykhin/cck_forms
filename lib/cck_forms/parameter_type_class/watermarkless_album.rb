cd58cbc2cbbb242b0335b3d65498701fd487f2cf
Bekseitov <bekseitov92@gmail.com>
2017-03-06 12:37:32 +0600

    Переодределение метода name для watermarkless_image/album

--- lib/cck_forms/parameter_type_class/watermarkless_album.rb
+++ lib/cck_forms/parameter_type_class/watermarkless_album.rb
@@ -1,6 +1,10 @@
 # Album without watermark. A subclass of Album.
 #
 class CckForms::ParameterTypeClass::WatermarklessAlbum < CckForms::ParameterTypeClass::Album
+  def self.name
+    'Альбом без водяного знака'
+  end
+
   def cck_image_type
     CckForms::ParameterTypeClass::WatermarklessImage
   end
--- lib/cck_forms/parameter_type_class/watermarkless_image.rb
+++ lib/cck_forms/parameter_type_class/watermarkless_image.rb
@@ -1,6 +1,10 @@
 # Image without watermark. A subclass of Image.
 #
 class CckForms::ParameterTypeClass::WatermarklessImage < CckForms::ParameterTypeClass::Image
+  def self.name
+    'Картинка без водяного знака'
+  end
+
   def self.additional_file_attributes
     {no_wm: '1'}
   end
